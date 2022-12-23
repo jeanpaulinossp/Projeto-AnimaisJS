87b0b29fab3ecf10a613ba8d964ceb61a9a6d69b
jeanpaulino.ssp <jean.ssparaiso@gmail.com>
2022-12-23 15:31:50 -0300

    Conclusão do Projeto Animais Fantásticos

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -14,12 +14,13 @@ export default class MenuMobile {
     this.openMenu = this.openMenu.bind(this);
   }
 
-  openMenu() {
-    this.menuList.classList.toggle(this.activeClass);
-    this.menuButton.classList.toggle(this.activeClass);
+  openMenu(event) {
+    event.preventDefault();
+    this.menuList.classList.add(this.activeClass);
+    this.menuButton.classList.add(this.activeClass);
     outsideClick(this.menuList, this.events, () => {
-      this.menuList.classList.toggle(this.activeClass);
-      this.menuButton.classList.toggle(this.activeClass);
+      this.menuList.classList.remove(this.activeClass);
+      this.menuButton.classList.remove(this.activeClass);
     });
   }
 
